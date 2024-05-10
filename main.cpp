5342fa71047dc031960ab8642fac0f57741ab274
MrYaz <LehaHotPlay@yandex.ru>
2024-05-11 02:32:12 +0500

    Сделанна основа

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+double hours = 0;
+double money = 0;
+
+int main()
+{
+    std::cout << " " << std::endl;
+    std::cout << "-----" << std::endl;
+    std::cout << "Введите количество времени которое вы работаете (в часах): ";
+    std::cin >> hours;
+    std::cout << "Введите количество денег которое вы заработали (в рублях): ";
+    std::cin >> money;
+    std::cout << " " << std::endl;
+    std::cout << "-----" << std::endl;
+    std::cout << "В час в зарабатываете: " << (money / hours);
+    std::cout << " " << std::endl;
+    std::cout << "-----" << std::endl;
+    std::cout << " " << std::endl;
+}
